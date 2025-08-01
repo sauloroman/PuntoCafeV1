da8a90128c4ca31e6074b80ceb9c1f51f61dc8b4
Humberto <camposhernandezjoshuahumberto@gmail.com>
2025-08-01 16:03:06 -0600

    Util constants

---
+++ src/utils/constants/ViewConstants.java
@@ -0,0 +1,17 @@
+package utils.constants;
+
+public final class ViewConstants {
+    public static String mainColor = "#263358";
+    public static String menuActiveBtn = "#1D7CAC";
+    public static String menuDeactiveBtn = "#263358";
+    public static String titleColor = "#377B4C";
+    public static String textBtn = "#121615";
+    public static String errorColor = "#ff6b6b";
+    public static String updateColor = "#fff3bf";
+    public static String purpleColor = "#70448e8";
+    public static String blueColor = "#E7F1F3";
+    public static String skyColor = "#3183F1";
+    public static String whiteColor = "#CCCCCC";
+    public static String blackColor = "#000000";
+}
+
